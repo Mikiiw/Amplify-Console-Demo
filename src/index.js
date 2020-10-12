d8d32ca6e63999a5398d00e657013a0ecf2d156c
Mickey Wang <32526912+Mikiiw@users.noreply.github.com>
2020-10-12 14:41:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render("Hello World", document.querySelector('#root'))
